56cf24525f15c44f29420534022b4211040c4983
Brianna Salinas <zevensos@gmail.com>
2026-05-07 20:26:40 -0500

    chore: initial angular project

--- brandradar-frontend/src/app/features/infrastructure-health/presentation/pages/system-health/system-health.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-system-health',
-  imports: [],
-  templateUrl: './system-health.html',
-  styleUrl: './system-health.css',
-})
-export class SystemHealth {}
