d6e4a0878c344ae5415c309bcf4d56ab3646e729
Brianna Salinas Guzmán <zevensos@gmail.com>
2026-06-20 02:52:04 -0500

    Delete brandradar-frontend directory

--- brandradar-frontend/src/app/features/crisis-detection/presentation/pages/incidents/incidents.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-incidents',
-  imports: [],
-  templateUrl: './incidents.html',
-  styleUrl: './incidents.css',
-})
-export class Incidents {}
--- brandradar-frontend/src/app/shared/directives/role-visibility.ts
+++
@@ -1,8 +0,0 @@
-import { Directive } from '@angular/core';
-
-@Directive({
-  selector: '[appRoleVisibility]',
-})
-export class RoleVisibility {
-  constructor() {}
-}
